f4000c4a11b2093a349856ee438c20ea85963fe8
Ivan Amoshyi <amoshyiivan@gmail.com>
2025-05-18 21:41:45 +0300

    released file upload route

--- server.js
+++ server.js
@@ -1,11 +1,51 @@
 const express = require("express");
+const multer = require("multer");
 const cors = require("cors");
+const fs = require("fs/promises");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+const uploadDir = path.join(process.cwd(), "temp");
 
-const PORT = 3000;
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, uploadDir);
+	},
+	filename: function (req, file, cb) {
+		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+		cb(null, file.originalname);
+	},
+});
+
+const upload = multer({ storage });
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static("public/stamps"));
+
+app.post("/stamp/upload", upload.single('stamp'), async (req, res, next) => {
+  const { path: tempPath, originalname } = req.file;
+
+  const newPath = path.join(__dirname, "public", "stamps", originalname);
+
+  await fs.rename(tempPath, newPath);
+  console.log(newPath);
+
+  res.status(201).json({
+    message: "success",
+		originalname
+	});
+})
+
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send("Something broke!");
+});
+
+const {PORT} = process.env;
 
 app.listen(PORT, () => {
   console.log('web server is running on port 3000');
